bba3d0defb8bc19c2ff8a5e2b7ffee1a7a176c42
fabjoncela <fabjoncela@gmail.com>
2024-09-18 17:25:09 +0200

    buttons have the same useState

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,29 @@ import { useState } from 'react';
 
 
 function App() {
-  return (
-    <>
-      <div className="main-div">
-        <h1 className="h1-text">Counters that update separately</h1>
-        <div className='btn1'>
-          <MyButton /></div>
-        <div className='fabi'>
-          <MyButton /></div>
-      </div>
-    </>
-  )
-}
-
-function MyButton() {
   const [count, setCount] = useState(0);
 
   function handleClick() {
     setCount(count + 1);
   }
   return (
-    <button onClick={handleClick}>
+    <>
+      <div className="main-div">
+        <h1 className="h1-text">Counters that update together</h1>
+        <div className='btn1'>
+          <MyButton count={count} onClick={handleClick}/></div>
+        <div className='fabi'>
+          <MyButton count={count} onClick={handleClick}/></div>
+      </div>
+    </>
+  )
+}
+
+function MyButton({count, onClick}) {
+ 
+
+  return (
+    <button onClick={onClick}>
       Clicked {count} times
     </button>
   );
