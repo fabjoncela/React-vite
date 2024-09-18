8d70c217034da28f9066c335746bced01cef42ef
fabjoncela <fabjoncela@gmail.com>
2024-09-18 14:52:16 +0200

    mini Count project

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
 
 import './App.css'
+import { useState } from 'react';
 
-const products = [
-  { title: 'Cabbage', isFruit: false, id: 1 },
-  { title: 'Garlic', isFruit: false, id: 2 },
-  { title: 'Apple', isFruit: true, id: 3 },
-];
+
 
 function App() {
-
-
-  const listItems = products.map(product =>
-    <li
-        key={product.id}
-        style={{
-            color: product.isFruit ? 'magenta' : 'darkgreen'
-        }}
-    >
-        {product.title}
-    </li>
-);
-
 return (
     <>
-        <ul>{listItems}</ul>
+        <div>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
+    </div>
     </>
 )
+}
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  );
 }
 
 export default App
