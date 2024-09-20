0bbd82b00563911985b9aab57a65cece673ec592
fabjoncela <fabjoncela@gmail.com>
2024-09-20 12:54:40 +0200

    React Componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
-
-import './App.css'
-
-const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
-};
+import Header from "./Header"
+import Card from "./Card.jsx";
 
 function App() {
 
 
   return (
     <>
-      <img className="avatar" src={user.imageUrl} alt={'Photo of ' + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize
-        }}
-      />
+      <Card />
+      <Card />
 
-      <h1>{user.name}</h1>
-      <p>Hello there.<br />How do you do?</p>
     </>
-  )
+  );
 }
 
 export default App
