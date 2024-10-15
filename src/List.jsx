5e931035799959fa9b363744946ed012c585f380
fabjoncela <fabjoncela@gmail.com>
2024-10-16 00:14:57 +0200

    Making list a reusable component

--- src/List.jsx
+++ src/List.jsx
@@ -1,24 +1,10 @@
 
-function List() {
+function List(props) {
 
-    const fruits = [
-        { id: 1, name: "apple", calories: 95 },
-        { id: 2, name: "orange", calories: 45 },
-        { id: 3, name: "banana", calories: 105 },
-        { id: 4, name: "coconut", calories: 159 },
-        { id: 5, name: "pineapple", calories: 37 },
-    ];
+    const itemList = props.items;
 
-    //fruits.sort((a, b) => a.name.localeCompare(b.name)); //alphabetical order
-    //fruits.sort((a, b) => b.name.localeCompare(a.name)); //reverse alphabetical order
-    //fruits.sort((a, b) => a.calories - b.calories); //numeric order
-    //fruits.sort((a, b) => b.calories - a.calories); //reverse numeric order
-
-    // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100)
-    //const highCalFruits = fruits.filter(fruit => fruit.calories < 100)
-
-    const listItems = fruits.map(fruit => <li key={fruit.id}>
-        {fruit.name}: {fruit.calories}</li>);
+    const listItems = itemList.map(item => <li key={item.id}>
+        {item.name}: <b>{item.calories}</b></li>);
 
     return (<ol>{listItems}</ol>);
 }
