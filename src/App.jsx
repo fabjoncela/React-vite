7062d71c1bcf2758de7aa6ae78009bcaaf057faa
fabjoncela <fabjoncela@gmail.com>
2024-10-16 00:19:05 +0200

    reusing list component

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,18 @@ function App() {
     { id: 4, name: "coconut", calories: 159 },
     { id: 5, name: "pineapple", calories: 37 },
   ];
+  const vegetables = [
+    { id: 6, name: "veg1", calories: 95 },
+    { id: 7, name: "qep", calories: 45 },
+    { id: 8, name: "patellxhan", calories: 105 },
+    { id: 9, name: "domate", calories: 159 },
+    { id: 10, name: "sallat", calories: 37 },
+  ];
 
   return (
     <>
-      <List items={fruits} category="Fruits"/>
+      <List items={fruits} category="Fruits" />
+      <List items={vegetables} category="Vegetables" />
 
 
     </>
--- src/List.jsx
+++ src/List.jsx
@@ -1,11 +1,15 @@
 
 function List(props) {
 
+    const category = props.category;
     const itemList = props.items;
 
     const listItems = itemList.map(item => <li key={item.id}>
         {item.name}: <b>{item.calories}</b></li>);
 
-    return (<ol>{listItems}</ol>);
+    return (<>
+    <h3>{category}</h3>
+    <ol>{listItems}</ol>
+    </>);
 }
 export default List
