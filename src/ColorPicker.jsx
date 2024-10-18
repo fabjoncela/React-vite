f8c9538e443c9642a0030635dff9680cc609ebed
fabjoncela <fabjoncela@gmail.com>
2024-10-19 00:57:20 +0200

    better design

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -1,27 +1,32 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 
-function ColorPicker(){
+function ColorPicker() {
 
     const [color, setColor] = useState('#FFFFFF')
 
-    function handleColorChange(e){
+    function handleColorChange(e) {
         setColor(e.target.value)
     }
 
 
-    return(<>
-    <div className='color-picker-container'>
-        <h1>Color Picker</h1>
-    <div className='color-display' >
-        <p>Selected color: {color}</p>
+    return (<>
+        <div className='color-picker-container'>
+            <h1>Color Picker</h1>
+            <div className='color-display' >
+                <p>Selected color: <span style={{ color: color }}>{color}</span></p>
+            </div>
+            <div className='color-box' style={{ backgroundColor: color }}></div>
+            <div className='color-selector'>
+                <label htmlFor="colorInput">Select a Color:</label>
+                <input
+                    id="colorInput"
+                    type="color"
+                    value={color}
+                    onChange={handleColorChange}
+                />
+            </div>
         </div>
-        <div className='color-box'style={{backgroundColor: color}}></div>
-        <div className='color-selector'>
-        <label htmlFor="">Select a Color:</label>
-        <input type="color" value={color} onChange={handleColorChange}/>
-        </div>
-    </div>
     </>)
 }
 export default ColorPicker
