5e459b17b679456f1f366d8e7f116e4637c507a2
fabjoncela <fabjoncela@gmail.com>
2024-10-19 01:37:02 +0200

    meh

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -3,13 +3,11 @@ import React, { useState } from 'react';
 
 function ColorPicker() {
 
-    const [color, setColor] = useState('#FFFFFF')
+    const [color, setColor] = useState('#000000')
 
     function handleColorChange(e) {
         setColor(e.target.value)
     }
-
-
     return (<>
         <div className='color-picker-container'>
             <h1>Color Picker</h1>
